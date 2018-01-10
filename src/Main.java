6d159d2b46c98fbb64429d6d826792b8f11054a3
mats- <mats.10@hotmail.no>
2018-01-10 11:51:02 +0100

    Test fra nytt lokalt repo

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("økdw");
+        System.out.println("test");
     }
 }
